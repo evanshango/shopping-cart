0b8a78404ecbcab5d6f737461470a34ed37bb0ec
Evans Shango <lutatwaevans@gmail.com>
2020-01-22 02:56:39 +0300

    implement user registration with passport

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var Product = require('../models/product');
 var csrf = require('csurf');
+var passport = require('passport');
+var Product = require('../models/product');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
@@ -9,21 +10,28 @@ router.use(csrfProtection);
 /* GET home page. */
 router.get('/', function (req, res, next) {
     Product.find(function (err, docs) {
-      var productChunks = [];
-      var chuckSize = 3;
-      for (var i = 0; i < docs.length; i += chuckSize) {
-        productChunks.push(docs.slice(i, i + chuckSize))
-      }
-      res.render('shop/index', {title: 'Shopping Cart', products: productChunks});
+        var productChunks = [];
+        var chuckSize = 3;
+        for (var i = 0; i < docs.length; i += chuckSize) {
+            productChunks.push(docs.slice(i, i + chuckSize))
+        }
+        res.render('shop/index', {title: 'Shopping Cart', products: productChunks});
     });
 });
 
 router.get('/user/signup', function (req, res, next) {
-    res.render('user/signup', {csrfToken: req.csrfToken()});
+    var messages = req.flash('error');
+    res.render('user/signup', {csrfToken: req.csrfToken(), messages, hasErrors: messages.length > 0});
 });
 
-router.post('/user/signup', function (req, res, next) {
-    res.redirect('/');
+router.post('/user/signup', passport.authenticate('local.signup', {
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signup',
+    failureFlash: true
+}));
+
+router.get('/profile', function (req, res, next) {
+    res.render('user/profile')
 });
 
 module.exports = router;
