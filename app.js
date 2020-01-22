53382410814a10a40b6cf27ad45f1110c443179f
Evans Shango <lutatwaevans@gmail.com>
2020-01-23 01:41:30 +0300

    implement cart views and add methods to store user cart items to session

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let session = require('express-session');
 let passport = require('passport');
 let flash = require('connect-flash');
 let validator = require('express-validator');
+let MongoStore = require('connect-mongo')(session);
 
 let indexRouter = require('./routes/index');
 let userRouter = require('./routes/user');
@@ -24,37 +25,42 @@ app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret: 'theflash', resave: false, saveUninitialized: false}));
+app.use(session({
+    secret: 'theflash', resave: false, saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection}),
+    cookie: {maxAge: 180 * 60 * 1000}
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-  res.locals.login = req.isAuthenticated();
-  next();
+    res.locals.login = req.isAuthenticated();
+    res.locals.session = req.session;
+    next();
 });
 
 app.use('/user', userRouter);
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+let Cart = require('../models/cart');
 let Product = require('../models/product');
 
 /* GET home page. */
@@ -12,6 +13,29 @@ router.get('/', function (req, res, next) {
         }
         res.render('shop/index', {title: 'Shopping Cart', products: productChunks});
     });
+});
+
+router.get('/add-to-cart/:id', function (req, res, next) {
+   let productId = req.params.id;
+   let cart = new Cart(req.session.cart ? req.session.cart : {});
+
+   Product.findById(productId, function (err, product) {
+       if (err){
+           return res.redirect('/')
+       }
+       cart.add(product, product.id);
+       req.session.cart = cart;
+       console.log(req.session.cart);
+       res.redirect('/');
+   })
+});
+
+router.get('/shopping-cart', function (req, res, next) {
+    if (!req.session.cart){
+        return res.render('shop/shopping-cart', {products: null})
+    }
+    let cart = new Cart(req.session.cart);
+    res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
 module.exports = router;
